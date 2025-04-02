57a8599af187d1ebc25f0b42b56b1bba03ea7f8c
ysj <411367308@qq.com>
2025-04-02 13:33:57 +0800

    test#1 commit

--- package/plugin/src/index.ts
+++ package/plugin/src/index.ts
@@ -1,7 +1,64 @@
 import type { IApi } from 'umi';
+import path from 'path';
 
 export default (api: IApi) => {
   // See https://umijs.org/docs/guides/plugins
-  console.log('plugin-demo');
-  
+  const config = api.userConfig;
+
+  api.describe({
+    key: 'keepaliveTabs',
+    config: {
+      schema(joi) {
+        return joi.string().required();
+      },
+      onChange: api.ConfigChangeType.regenerateTmpFiles,
+    },
+    enableBy: api.EnableBy.config,
+  });
+
+  api.addRuntimePluginKey(() => 'keepaliveTabs');
+
+  api.onGenerateFiles(() => {
+    api.writeTmpFile({
+      content: `export const add = (num1: number, num2: number): number => num1 + num2`,
+      path: 'index.tsx',
+    })
+    api.writeTmpFile({
+      content: `import React, { useEffect } from 'react';
+      import { ApplyPluginsType } from 'umi';
+      import { getPluginManager } from '../core/plugin';
+      
+      function Layout(props) {
+      
+        async function getRuntime() {
+          const config = await getPluginManager().applyPlugins({
+            key: 'keepaliveTabs',
+            type: ApplyPluginsType.modify,
+            initialValue: '',
+            async: true,
+          });
+          setTimeout(() => {
+          document.title = config || "${api.userConfig.keepaliveTabs}";
+          }, 1000);
+        }
+      
+        useEffect(() => {
+          getRuntime();
+        }, [])
+      
+        return (
+          <div>{props.children}</div>
+        )
+      }
+      export function rootContainer(container) {
+        return React.createElement(Layout, null, container);
+      }      
+      `,
+      path: 'runtime.tsx',
+    });
+  })
+
+  api.addRuntimePlugin(() => path.join(api.paths.absTmpPath!, `plugin-keepaliveTabs/runtime.tsx`),)
+
+  console.log(config.keepaliveTabs);
 };
